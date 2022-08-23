52f557bf293fb05635c905d2eff4ae4ce782795b
nacho503 <ignacio.rosenberg.503@gmail.com>
2022-08-23 17:50:26 -0400

    logo responsivo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+  NavLogo,
+} from "./NavbarElements";
+import Logo from "./Logo.JPG";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <NavLogo src={Logo}></NavLogo>
+          {/* <img src={Logo} alt="Logo de la app Handey"></img> */}
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/nosotros">Nosotros</NavLink>
+          <NavLink to="/contacto">Contáctanos</NavLink>
+          <NavLink to="/registrate">Registrate</NavLink>
+          <NavBtn>
+            <NavBtnLink to="/ingresar">Ingresar</NavBtnLink>
+          </NavBtn>
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
